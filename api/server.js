6b4abe3fbe79e8f6a7215135f333096cb7ef5ce3
Oscar Rojas <rojasoscar752@gmail.com>
2025-03-31 13:57:30 -0500

    arreglar docker.compose.yml

--- api/server.js
+++ api/server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   res.json({ message: 'API funcionando con Express.js' });
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor API corriendo en http://localhost:${PORT}`);
 });
